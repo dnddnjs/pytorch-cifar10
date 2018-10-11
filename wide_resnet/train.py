e15852d07cffb383b1377504fb0b801a9d5d9437
dnddnjs <dnddnjs11@naver.com>
2018-10-11 10:10:40 +0900

    add wide resnet

---
+++ wide_resnet/train.py
@@ -0,0 +1,138 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torch.backends.cudnn as cudnn
+
+import torchvision
+import torchvision.transforms as transforms
+
+from model import wide_resnet
+import argparse
+
+parser = argparse.ArgumentParser(description='cifar10 classification models')
+parser.add_argument('--lr', default=0.1, help='')
+parser.add_argument('--resume', default=None, help='')
+parser.add_argument('--batch_size', default=128, help='')
+parser.add_argument('--num_worker', default=4, help='')
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print('==> Preparing data..')
+transforms_train = transforms.Compose([
+	transforms.RandomCrop(32, padding=4),
+	transforms.RandomHorizontalFlip(),
+	transforms.ToTensor(),
+	transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+transforms_test = transforms.Compose([
+	transforms.ToTensor(),
+	transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+dataset_train = torchvision.datasets.CIFAR10(root='../data', train=True, download=True, transform=transforms_train)
+dataset_test = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transforms_test)
+train_loader = torch.utils.data.DataLoader(dataset_train, batch_size=args.batch_size, 
+	                      shuffle=True, num_workers=args.num_worker)
+test_loader = torch.utils.data.DataLoader(dataset_test, batch_size=100, 
+	                     shuffle=False, num_workers=args.num_worker)
+
+# there are 10 classes so the dataset name is cifar-10
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 
+	       'dog', 'frog', 'horse', 'ship', 'truck')
+
+print('==> Making model..')
+
+net = wide_resnet()
+net = net.to(device)
+if device == 'cuda':
+	net = torch.nn.DataParallel(net)
+	cudnn.benchmark = True
+
+if args.resume is not None:
+	checkpoint = torch.load('./save_model/' + args.resume)
+	net.load_state_dict(checkpoint['net'])
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.1, 
+	                  momentum=0.9, weight_decay=5e-4)
+step_lr_scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[60, 120, 160], gamma=0.2)
+
+
+def train(epoch):
+	net.train()
+
+	train_loss = 0
+	correct = 0
+	total = 0
+
+	for batch_idx, (inputs, targets) in enumerate(train_loader):
+		inputs = inputs.to(device)
+		targets = targets.to(device)
+		outputs = net(inputs)
+		loss = criterion(outputs, targets)
+
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+
+		train_loss += loss.item()
+		_, predicted = outputs.max(1)
+		total += targets.size(0)
+		correct += predicted.eq(targets).sum().item()
+		print('epoch : {} [{}/{}]| loss: {:.3f} | acc: {:.3f}'.format(epoch, batch_idx, 
+			  len(train_loader), train_loss/(batch_idx+1), 100.*correct/total))
+
+
+
+def test(epoch, best_acc):
+	net.eval()
+
+	test_loss = 0
+	correct = 0
+	total = 0
+
+	with torch.no_grad():
+		for batch_idx, (inputs, targets) in enumerate(test_loader):
+			inputs = inputs.to(device)
+			targets = targets.to(device)
+			outputs = net(inputs)
+			loss = criterion(outputs, targets)
+
+			test_loss += loss.item()
+			_, predicted = outputs.max(1)
+			total += targets.size(0)
+			correct += predicted.eq(targets).sum().item()
+
+			print('epoch : {} [{}/{}]| loss: {:.3f} | acc: {:.3f}'.format(epoch, batch_idx, 
+			  len(test_loader), test_loss/(batch_idx+1), 100 * correct/total))
+
+	acc = 100 * correct / total
+
+	if acc > best_acc:
+		print('==> Saving model..')
+		state = {
+		    'net': net.state_dict(),
+		    'acc': acc,
+		    'epoch': epoch,
+		}
+		if not os.path.isdir('save_model'):
+		    os.mkdir('save_model')
+		torch.save(state, './save_model/ckpt.pth')
+		best_acc = acc
+
+	return best_acc
+
+
+if __name__=='__main__':
+	best_acc = 0
+	if args.resume is None:
+		for epoch in range(200):
+			step_lr_scheduler.step()
+			train(epoch)
+			best_acc = test(epoch, best_acc)
+			print('best test accuracy is ', best_acc)
+	else:
+		test(epoch=0, best_acc=0)
